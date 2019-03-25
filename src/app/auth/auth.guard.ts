a65e0f8716c444a209febde2fbdb8356113123f3
Ying Wang <y2554wan@uwaterloo.ca>
2019-03-25 18:14:58 -0400

    Implemented lazy loading for auth module (login and signup)

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -22,7 +22,7 @@ export class AuthGuard implements CanActivate {
       if (isAuth) {
         return true;
       }
-      this.router.navigate(['/login']);
+      this.router.navigate(['/auth/login']);
       return isAuth;
     }
 
