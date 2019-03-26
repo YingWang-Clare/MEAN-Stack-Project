8d066cdfe62abd5dc387cd72238c53e4f871246c
Ying Wang <y2554wan@uwaterloo.ca>
2019-03-26 11:14:39 -0400

    Add nodemon.json and environment.js to set up global configuration.

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,41 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const postsRoutes = require('./routes/posts');
-const userRoutes = require('./routes/users');
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/users");
 
 const app = express();
 
-
-mongoose.connect("mongodb+srv://ying:YPQ7CgX15FZr6Ajj@cluster0-bdrje.mongodb.net/node-angular")
-.then(() => {
-  console.log('Connected to database!');
-})
-.catch(() => {
-  console.log('Connection failed!');
-});
+mongoose
+  .connect(
+    "mongodb+srv://ying:" +
+      process.env.MONGO_ATLAS_PW +
+      "@cluster0-bdrje.mongodb.net/node-angular"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false})); // optional
+app.use(bodyParser.urlencoded({ extended: false })); // optional
+
 app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers',
-   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-   );
-   res.setHeader('Access-Control-Allow-Methods',
-   'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -5,6 +5,9 @@ import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 import { Post } from './post.model';
+import { environment } from '../../environments/environment';
+
+const BACKEND_URL = environment.apiUrl + '/posts/';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -17,7 +20,7 @@ export class PostsService {
     const queryParams = `?pageSize=${postsPerPage}&page=${currentPage}`;
     this.http
       .get<{ message: string; posts: any; maxPosts: number }>(
-        'http://localhost:3000/api/posts' + queryParams)
+        BACKEND_URL + queryParams)
       .pipe(
         map(postData => {
           return {
@@ -54,7 +57,7 @@ export class PostsService {
       imagePath: string,
       creator: string
     }>(
-      'http://localhost:3000/api/posts/' + id
+      BACKEND_URL + id
     );
   }
 
@@ -65,7 +68,7 @@ export class PostsService {
     postData.append('image', image, title);
     this.http
       .post<{ message: string; post: Post }>(
-        'http://localhost:3000/api/posts',
+        BACKEND_URL,
         postData
       )
       .subscribe(responseData => {
@@ -85,7 +88,7 @@ export class PostsService {
       postData = { id, title, content, imagePath: image, creator: null };
     }
     this.http
-      .put('http://localhost:3000/api/posts/' + id, postData)
+      .put(BACKEND_URL + id, postData)
       .subscribe(response => {
         this.router.navigate(['/']);
       });
@@ -93,6 +96,6 @@ export class PostsService {
 
   deletePost(postId: string) {
     return this.http
-      .delete('http://localhost:3000/api/posts/' + postId);
+      .delete(BACKEND_URL + postId);
   }
 }
